feeb66e46bcb46c7c272b75ae58e04df8d9ad7b3
Supamit2545 <mairutv@gmail.com>
2024-07-07 06:52:11 +0700

    Second Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { document } from 'postcss'
 import './App.css'
 import Certificate from './Components/Certificate'
 import Context from './Components/Context'
@@ -7,29 +6,35 @@ import Services from './Components/Services'
 import Topbar from './Components/Topbar'
 import Projects from './Components/projects'
 import ParticleBackground from './Components/particles/ParticleBackground'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+import { useEffect, useState } from 'react'
 
 function App() {
+  useEffect(()=>{
+    Aos.init({duration: 2000})
+  }, []);
   return (
     <div id='app'>
-      <ParticleBackground/>
-      <div className='tb'>
+      <div className='sticky'>
         <Topbar/>
       </div>
-      <div className='pf'>
+      <div className='' data-aos="fade-right">
         <Profile/>
       </div>
-      <div className='pj'>
+      <div className='' data-aos="fade-right">
         <Projects/>
       </div>
-      <div className='cc'>
+      <div className='' data-aos="fade-right">
         <Certificate/>
       </div>
-      <div className='sv'>
+      <div className='' data-aos="fade-left">
         <Services/>
       </div>
-      <div className='ct'>
+      <div className='' data-aos="fade-left">
         <Context/>
       </div>
+      <ParticleBackground/>
     </div>
   )
 }
